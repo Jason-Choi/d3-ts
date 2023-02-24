850c8151b41a6e694259b7aa15d90ca936525699
jason-choi-skku <jasonchoi3@g.skku.edu>
2023-02-24 21:56:35 +0900

    link fix

--- src/components/header.ts
+++ src/components/header.ts
@@ -3,7 +3,8 @@ import { html } from "lit";
 const template = () => html`
 <nav class="navbar flex-row bg-base-300 justify-between">
     <a class="btn btn-ghost normal-case text-xl">Visual Analytics</a>
-    <button class="btn">Github</button>
+    <button class="btn" @click=${()=> { window.open("https://github.com/Jason-Choi/reactive-d3-boilerplate")
+        }}>Github</button>
 </nav>
 `;
 
