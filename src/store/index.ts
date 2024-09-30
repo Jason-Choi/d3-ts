5d8b72ec540a3ed11aaee8ee0f5d85d734c9005f
Jiwon Choi <jiwnchoi@gmail.com>
2024-09-30 16:57:53 +0900

    Update index.ts

--- src/store/index.ts
+++ src/store/index.ts
@@ -6,9 +6,9 @@ export const selected = signal<number>(0);
 const prepareData = () => {
     const random = d3.randomNormal(0, 0.2);
     const sqrt3 = Math.sqrt(3);
-    const cluster1 = d3.range(50).map(() => [random() + sqrt3, random() + 1, 1]);
-    const cluster2 = d3.range(50).map(() => [random() - sqrt3, random() + 1, 2]);
-    const cluster3 = d3.range(50).map(() => [random(), random() - 1, 3]);
+    const cluster1 = d3.range(1000).map(() => [random() + sqrt3, random() + 1, 1]);
+    const cluster2 = d3.range(1000).map(() => [random() - sqrt3, random() + 1, 2]);
+    const cluster3 = d3.range(1000).map(() => [random(), random() - 1, 3]);
     return d3.shuffle([...cluster1, ...cluster2, ...cluster3]);
 }
 
